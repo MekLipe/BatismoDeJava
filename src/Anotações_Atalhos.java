b439b5785227cb4e5c3d1beeb8fd8576d037a912
MekLipe <lipe_kopper@hotmail.com>
2025-08-06 11:12:29 -0400

    :white_check_mark: test: Removendo arquvio Teste3

--- src/Anotações_Atalhos.java
+++ src/Anotações_Atalhos.java
@@ -3,7 +3,7 @@ public @interface Anotações_Atalhos {
     * Atalhos:
     * Fazer um construtor: Alt + Insert
     * Formatar o código automaticamente: Alt + Shift + F
-    * Procurar e mudar todas as instancias: Ctrl + H no pc e Ctrl + R no note
+    * Procurar e mudar todas as instancias: Ctrl + H no pc e Ctrl + R no note'
     *
     * Anotações:
     * toda ves que voce for sobrecarregar um construtor, basta referenciar oq ja foi referenciado com
